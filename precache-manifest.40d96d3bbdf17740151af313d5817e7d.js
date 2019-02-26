2d4ce6411ec4eac8e718c2a64291287aa0edd159
Artem <dunaev.aa@yandex.ru>
2019-02-26 08:26:36 +0500

    Updates

---
+++ precache-manifest.40d96d3bbdf17740151af313d5817e7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f473882bc3a64c119e13",
+    "url": "/faq/static/js/runtime~main.f473882b.js"
+  },
+  {
+    "revision": "97536b0f276377935404",
+    "url": "/faq/static/js/main.97536b0f.chunk.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/faq/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "97536b0f276377935404",
+    "url": "/faq/static/css/main.b72972ed.chunk.css"
+  },
+  {
+    "revision": "f46b0e31b63b8e5f5ae6702c0be5b668",
+    "url": "/faq/index.html"
+  }
+];
